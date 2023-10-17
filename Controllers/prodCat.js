a0e3c0ee97344a831af2328773ea373767d76c03
ashish <invoxico.ashish@gmail.com>
2023-10-17 14:45:01 +0530

    testing

--- Controllers/prodCat.js
+++ Controllers/prodCat.js
@@ -1,33 +1,30 @@
 const dataBase = require("../Config/dataBase")
-
-
 exports.Prodcate = async (req, res) => {
     let prodCatName = req.body.prodCatName;
     let prodCatParent = req.body.prodCatParent;
     let prodCatIsFeatured = req.body.prodCatIsFeatured;
     let prodCatActive = req.body.prodCatStatus;
     const dataToInsert = [prodCatName, prodCatParent, prodCatIsFeatured, prodCatActive];
-    const sqlQuery = `INSERT INTO tbl_products_categories (prodcat_name,prodcat_parent,prodcat_is_featured,prodcat_active) VALUES (?,?,?,?)`
+    const sqlQuery = `INSERT INTO tbl_products_categories (prodcat_name,prodcat_parent,prodcat_is_featured,prodcat_active) VALUES (?,?,?,?)`;
     if (!prodCatName || !prodCatParent || !prodCatIsFeatured || !prodCatActive) {
         return res.status(204).json({ success: false, message: "Content is Required" });
     };
     await dataBase.query(sqlQuery, dataToInsert, (error, response) => {
-        if (error) return res.status(400).json({ success: false, message: "Something went wrong", error })
+        if (error) return res.status(400).json({ success: false, message: "Something went wrong", error });
         return res.status(200).json({ success: true, message: "Successfully inserted", response });
     });
 };
 exports.ProdCateDelByID = async (req, res) => {
     const id = req.params.id;
-    const dataToInsert = [prodCatName, prodCatParent, prodCatIsFeatured, prodCatActive];
-    const sqlQuery = `UPDATE  tbl_products_categories SET prodcat_deleted = 1 WHERE prodcat_id = ${id}`;
-    if (!prodCatName || !prodCatParent || !prodCatIsFeatured || !prodCatActive) {
-        return res.status(400).json({ success: false, message: "fields are required" });
+    const sqlQuery = `UPDATE tbl_products_categories SET prodcat_deleted = 1, prodcat_updated_at = now() WHERE prodcat_id = ${id}`;
+    if (!id) {
+        return res.status(404).json({ success: false, message: "not found or id is required" });
     }
-    await dataBase.query(sqlQuery, dataToInsert, (err, response) => {
+    await dataBase.query(sqlQuery, (err, response) => {
         if (err) return res.status(400).json({ success: false, message: "Something went wrong", err });
         return res.status(200).json({ success: true, message: "Deleted", response });
     });
-}
+};
 exports.UpdateProdCateById = async (req, res) => {
     const id = req.params.id;
     let prodCatName = req.body.prodCatName;
@@ -35,18 +32,15 @@ exports.UpdateProdCateById = async (req, res) => {
     let prodCatIsFeatured = req.body.prodCatIsFeatured;
     let prodCatActive = req.body.prodCatStatus;
     const dataToInsert = [prodCatName, prodCatParent, prodCatIsFeatured, prodCatActive, id];
-    // const sqlQuery = `UPDATE tbl_products_categories SET prodcat_name = ?, prodcat_parent = ?, prodcat_is_featured = ?, prodcat_active = ?, WHERE prodcat_id = ${id}`;
-    const sqlQuery = `UPDATE tbl_products_categories SET prodcat_name = ?, prodcat_parent = ?, prodcat_is_featured = ?, prodcat_active = ? WHERE prodcat_id = ?`;
-    console.log(dataToInsert)
-    // return
+    const sqlQuery = `UPDATE tbl_products_categories SET prodcat_name = ?, prodcat_parent = ?, prodcat_is_featured = ?, prodcat_active = ?, prodcat_updated_at = now() WHERE prodcat_id = ?`;
     if (!prodCatName || !prodCatParent || !prodCatIsFeatured || !prodCatActive) {
         return res.status(204).json({ success: false, message: "Content is Required" });
     };
     await dataBase.query(sqlQuery, dataToInsert, (err, result) => {
         if (err) return res.status(400).json({ success: false, message: "failed", err });
-        return res.status(200).json({ success: true, message: "Updated successfully", result })
-    })
-}
+        return res.status(200).json({ success: true, message: "Updated successfully", result });
+    });
+};
 exports.singleProdCateById = async (req, res) => {
     const id = req.params.id;
     const sqlQuery = `SELECT prodcat_id,prodcat_name,prodcat_parent,prodcat_is_featured,prodcat_active AS prodcat_Status FROM  tbl_products_categories WHERE prodcat_id = ${id}`;
@@ -60,5 +54,15 @@ exports.productCategoryAll = async (req, res) => {
     await dataBase.query(sqlQurey, (error, response) => {
         if (error) return res.status(400).json({ success: false, message: "Something Went Wrong", error });
         return res.status(200).json({ success: true, message: "ok", response });
+    });
+};
+exports.delMultipleCateById = async (req, res) => {
+    const ids = req.query.id;
+    console.log(req.query.id)
+    return
+    const sqlQuery = `UPDATE tbl_products_categories SET prodcat_deleted = 1 WHERE prodcat_id IN (?)`;
+    dataBase.query(sqlQuery, [ids], (err, result) => {
+        if (err) return console.log(err)
+        console.log(result)
     })
 }
--- Router/deleteRoutes.js
+++ Router/deleteRoutes.js
@@ -3,6 +3,7 @@ const deleterouter = express.Router();
 const deleteController = require("../Controllers/prodCat")
 const Authentication = require("../middleware/authorization")
 
-deleterouter.put("/single/prod/cat/:id", Authentication, deleteController.ProdCateDelByID)
+deleterouter.put("/single/prod/cat/:id", deleteController.ProdCateDelByID)
+deleterouter.put("/multiple/prod/cat",deleteController.delMultipleCateById)
 
 module.exports = deleterouter
